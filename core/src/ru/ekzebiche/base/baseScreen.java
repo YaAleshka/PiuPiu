80e57dc6a5727d589d4e3abba2010679682cd770
YaAleshka <ashisora28@gmail.com>
2020-10-19 14:51:45 +0300

    звездолет

---
+++ core/src/ru/ekzebiche/base/baseScreen.java
@@ -0,0 +1,104 @@
+package ru.ekzebiche.base;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class baseScreen implements Screen, InputProcessor {
+
+    protected SpriteBatch batch;
+
+
+    @Override
+    public void show() {
+        System.out.println("show");
+        batch = new SpriteBatch();
+        Gdx.input.setInputProcessor(this);
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        System.out.println("resize width = "+ width+ "height =" + height);
+
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("pause");
+    }
+
+    @Override
+    public void resume() {
+        System.out.println("resume");
+
+    }
+
+    @Override
+    public void hide() {
+        System.out.println("hide");
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        System.out.println("dispose");
+        batch.dispose();
+    }
+
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("keyDown keycode = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        System.out.println("keyUp keycode = " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        System.out.println("keyUp character = " + character);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchDown screenX = " + screenX + "screenY = " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("touchUP screenX = " + screenX + "screenY = " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        System.out.println("touchDragged = " + screenX + "screenY = " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
+
--- core/src/ru/ekzebiche/piupiu.java
+++ core/src/ru/ekzebiche/piupiu.java
@@ -1,65 +1,13 @@
 package ru.ekzebiche;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class piupiu extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Texture img2;
-	Texture img3;
-	float point = 0f;
-	float point2 = 1503f;
+import ru.ekzebiche.Screen.MenuScreen;
+
+public class piupiu extends Game {
 
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		img = new Texture("space.jpg");
-		img2 = new Texture("flyspace.jpg");
-		img3 = new Texture("flyspace.jpg");
+			setScreen(new MenuScreen());
 	}
-
-	@Override
-	public void render() {
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.setColor(1f, 1f, 1f, 1f);
-		batch.draw(img, 0, 0);
-		batch.setColor(1f, 1f, 1f, 0.5f);
-		batch.draw(img2, point, 0);
-		point -= 1;
-		batch.setColor(1f, 1f, 1f, 0.5f);
-		batch.draw(img3, point2, 0);
-		point2 -= 1;
-		if (point2 == -100) {
-			point = 1603f;
-			batch.setColor(1f, 1f, 1f, 0.5f);
-			batch.draw(img3, point2, 0);
-			point2 -= 1;
-		}
-		if (point == -100) {
-			point2 = 1603f;
-			batch.setColor(1f, 1f, 1f, 0.5f);
-			batch.draw(img2, point, 0);
-			point -= 1;
-		}
-		batch.end();
-	}
-
-
-
-
-
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-		img2.dispose();
-		img3.dispose();
-	}
-
 }
